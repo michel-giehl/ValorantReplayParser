69ddb5db2f9fffeca1808b3bcee0c030f430933d
Michel Giehl <gimifehd@gmail.com>
2026-06-25 21:13:25 +0200

    Fix Export group path, add PlayerState export group

--- src/Replay.Valorant/Descriptors/ValorantExportGroups.cs
+++ src/Replay.Valorant/Descriptors/ValorantExportGroups.cs
@@ -61,9 +61,27 @@ internal sealed class RemoteCharacterUpdateDescriptor : ExportGroupDescriptor<Re
     }
 }
 
+internal sealed class BaseReplayPlayerState : ExportGroupDescriptor<BaseReplayPlayerState>
+{
+    public override string Path => "/Game/GameModes/Common/BaseReplayPlayerState.BaseReplayPlayerState_C";
+    public override ExportCategory Categories => ExportCategory.Movement;
+    public override ExportGroupKind Kind => ExportGroupKind.Unknown;
+
+    public uint RemoteRole { get; set; }
+    public uint Owner { get; set; }
+    public bool OnlySpectator { get; set; }
+
+    protected override void Configure()
+    {
+        AddProperty(x => x.RemoteRole).ObjectNetGuid();
+        AddProperty(x => x.Owner).ObjectNetGuid();
+        AddProperty("bOnlySpectator", x => x.OnlySpectator);
+    }
+}
+
 internal sealed class BaseReplayControllerDescriptor : ExportGroupDescriptor<BaseReplayControllerDescriptor>
 {
-    public override string Path => "/Game/Characters/BaseReplayController.BaseReplayController_C";
+    public override string Path => "/Game/Characters/_Core/BaseReplayController.BaseReplayController_C";
     public override ExportCategory Categories => ExportCategory.Movement;
     public override ExportGroupKind Kind => ExportGroupKind.PlayerController;
 
